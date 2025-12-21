559c4f5456b38e2703cca41d8e5e8662467cf78d
oTTeuM <123246079+oTTeuMsTudio@users.noreply.github.com>
2025-12-21 14:55:03 +0100

    Create queries.rs

---
+++ src/queries.rs
@@ -0,0 +1,99 @@
+use crate::messages::BevyMessageDuplex;
+use crate::signal_synced::{signal_synced, RwSignalSynced};
+use bevy::ecs::component::Mutable;
+use bevy::ecs::query::{QueryData, QueryFilter};
+use bevy::prelude::*;
+use paste::paste;
+use std::marker::PhantomData;
+use variadics_please::all_tuples;
+
+pub fn single_query_signal<D, F>() -> (RwSignalSynced<Option<D>>, BevyQueryDuplex<D, F>)
+where
+    for<'a> D: QueryDataOwned<'a> + Clone + Send + Sync + 'static,
+    F: QueryFilter,
+{
+    let (signal, duplex) = signal_synced(None);
+
+    (
+        signal,
+        BevyQueryDuplex {
+            duplex,
+            marker: PhantomData,
+        },
+    )
+}
+
+pub trait QueryDataOwned<'q> {
+    type Qdata: QueryData;
+
+    fn from_query_data<'w, 's>(data: &<Self::Qdata as QueryData>::Item<'w, 's>) -> Self;
+
+    fn set_query_data<'w, 's>(&self, data: &mut <Self::Qdata as QueryData>::Item<'w, 's>);
+
+    fn is_changed<'w, 's>(data: &<Self::Qdata as QueryData>::Item<'w, 's>) -> bool;
+}
+
+macro_rules! impl_as_query_data {
+    ($(#[$meta:meta])* $($name:ident),*) => {
+        $(#[$meta])*
+        impl<'q, $($name: bevy::prelude::Component<Mutability = Mutable> + Clone),*> QueryDataOwned<'q> for ($($name,)*) {
+            type Qdata = ($(&'q mut $name,)*);
+
+            fn from_query_data<'w, 's>(data: &<Self::Qdata as QueryData>::Item<'w, 's>) -> Self {
+                paste! {
+                    let ($([<$name:lower>],)*) = data;
+                    ($(
+                        (**[<$name:lower>]).clone(),
+                    )*)
+                }
+            }
+
+            fn set_query_data<'w, 's>(&self, data: &mut <Self::Qdata as QueryData>::Item<'w, 's>) {
+                paste! {
+                    let ($([<$name:lower>],)*) = data;
+                    let ($([<$name:lower _self>],)*) = self;
+
+                    $(
+                        **[<$name:lower>] = (*[<$name:lower _self>]).clone();
+                    )*
+                }
+            }
+
+            fn is_changed<'w, 's>(data: &<Self::Qdata as QueryData>::Item<'w, 's>) -> bool {
+                paste! {
+                    let ($([<$name:lower>],)*) = data;
+                    $(
+                        if [<$name:lower>].is_changed() {
+                            return true;
+                        }
+                    )*
+                    false
+                }
+            }
+        }
+    };
+}
+
+all_tuples!(impl_as_query_data, 1, 15, T);
+
+pub struct BevyQueryDuplex<D, F = ()>
+where
+    for<'a> D: QueryDataOwned<'a>,
+    F: QueryFilter,
+{
+    pub(crate) duplex: BevyMessageDuplex<Option<D>>,
+    marker: PhantomData<F>,
+}
+
+impl<D, F> Clone for BevyQueryDuplex<D, F>
+where
+    for<'a> D: QueryDataOwned<'a>,
+    F: QueryFilter,
+{
+    fn clone(&self) -> Self {
+        Self {
+            duplex: self.duplex.clone(),
+            marker: PhantomData,
+        }
+    }
+}
