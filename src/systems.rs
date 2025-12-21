cbae95711ab7a914b33b33c1749f9354947a7418
oTTeuM <123246079+oTTeuMsTudio@users.noreply.github.com>
2025-12-21 16:58:00 +0100

    Create systems.rs

---
+++ src/systems.rs
@@ -0,0 +1,121 @@
+use crate::messages::BevyMessageDuplex;
+use crate::prelude::QueryDataOwned;
+use crate::traits::{HasReceiver, HasSender};
+use bevy::ecs::message::MessageId;
+use bevy::ecs::query::QueryFilter;
+use bevy::prelude::*;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone, Copy)]
+pub struct SyncSignalResourceSet;
+
+#[cfg(feature = "bevy_state")]
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone, Copy)]
+pub struct SyncSignalStateSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone, Copy)]
+pub struct ImportLeptosMessageSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone, Copy)]
+pub struct ExportLeptosMessageSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone, Copy)]
+pub struct SyncQuerySet;
+
+#[derive(Resource, Deref, DerefMut)]
+pub struct ImportedMessageIds<M: Message>(Vec<MessageId<M>>);
+
+impl<M: Message> Default for ImportedMessageIds<M> {
+    fn default() -> Self {
+        Self(Vec::with_capacity(4))
+    }
+}
+
+pub fn import_and_send_leptos_messages<R, M>(
+    rx: Res<R>,
+    mut imported_message_ids: ResMut<ImportedMessageIds<M>>,
+    mut message_writer: MessageWriter<M>,
+) where
+    R: HasReceiver<M> + Resource,
+    M: Message,
+{
+    imported_message_ids.clear();
+
+    for message in rx.rx().try_iter() {
+        let message_id = message_writer.write(message);
+        imported_message_ids.push(message_id);
+    }
+}
+
+pub fn read_and_export_leptos_messages<S, M>(
+    tx: Res<S>,
+    imported_message_ids: Res<ImportedMessageIds<M>>,
+    mut message_reader: MessageReader<M>,
+) where
+    S: HasSender<M> + Resource,
+    M: Message + Clone,
+{
+    for (message, id) in message_reader.read_with_id() {
+        if !imported_message_ids.contains(&id) {
+            tx.tx().send(message.clone()).unwrap();
+        }
+    }
+}
+
+pub fn sync_signal_resource<D, R>(mut resource: ResMut<R>, sync: Res<D>)
+where
+    R: Resource + Clone,
+    D: HasReceiver<R> + HasSender<R> + Resource,
+{
+    if resource.is_changed() && !resource.is_added() {
+        sync.tx().send(resource.clone()).unwrap();
+    }
+
+    for event in sync.rx().try_iter() {
+        *resource = event;
+    }
+}
+
+#[cfg(feature = "bevy_state")]
+pub fn sync_signal_state<D, S>(mut state: ResMut<State<S>>, sync: Res<D>)
+where
+    S: bevy::state::state::FreelyMutableState + Clone,
+    D: HasReceiver<S> + HasSender<S> + Resource,
+{
+    if state.is_changed() && !state.is_added() {
+        sync.tx().send(state.clone()).unwrap();
+    }
+
+    for event in sync.rx().try_iter() {
+        *state = State::new(event);
+    }
+}
+
+pub fn sync_query<D, F>(
+    duplex: Res<BevyMessageDuplex<Option<D>>>,
+    mut query: Query<<D as QueryDataOwned>::Qdata, F>,
+    mut prev_some: Local<bool>,
+) where
+    for<'a> D: QueryDataOwned<'a> + Send + Sync + 'static,
+    F: QueryFilter,
+{
+    let mut item = query.single_mut().ok();
+
+    let changed = if let Some(item) = &item {
+        !*prev_some || D::is_changed(item)
+    } else {
+        *prev_some
+    };
+
+    *prev_some = item.is_some();
+
+    if changed {
+        let item = item.map(|item| D::from_query_data(&item));
+        duplex.tx().send(item).unwrap();
+    } else {
+        for message in duplex.rx().try_iter() {
+            if let (Some(message), Some(item)) = (message, &mut item) {
+                message.set_query_data(item);
+            }
+        }
+    }
+}
